9de86f3bcb7f141c4277ea25d97c73cfffded745
Purp1eFox <84629271+Purp1eFox@users.noreply.github.com>
2021-06-04 17:50:01 +0200

    Update avatar.js
    Commentaires

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,12 +1,12 @@
 const { MessageEmbed } = require('discord.js')
 
 exports.run = async (_client, message) => {
-    const user = message.mentions.users.first() || message.author;
-    const avatarEmbed = new MessageEmbed()
+    const user = message.mentions.users.first() || message.author; // On récupère le membre dont on veut l'avatar
+    const avatarEmbed = new MessageEmbed() // On créé l'embed
         .setColor('#3867d6')
         .setAuthor('Avatar de : ' + user.username)
         .setImage(user.avatarURL({ format: 'png', dynamic: true, size: 1024 }));
-    message.channel.send(avatarEmbed);
+    message.channel.send(avatarEmbed); // On l'envoie
 }
 
 exports.help = {
