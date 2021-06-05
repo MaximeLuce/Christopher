cbb42c1d04d4b80d9b22febb647d55f55416455e
Purp1eFox <84629271+Purp1eFox@users.noreply.github.com>
2021-06-05 21:48:33 +0200

    Create unmute.js
    Ajout unmute

---
+++ commands/unmute.js
@@ -0,0 +1,31 @@
+const { MessageEmbed } = require("discord.js");
+
+exports.run = async (client, message, args) => {
+    if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("Que voulais-tu faire ? Il n'y a rien à voir ici !")
+
+        if(message.mentions.users.size === 0) return message.channel.send("Tu dois mentionner un utilisateur !");
+        
+        const mute = message.guild.member(message.mentions.users.first());
+
+        if(!mute) return message.channel.send('Cet utilisateur n\'existe pas !')
+
+        try{
+            mute.send("Vous avez été unmute sur le Max de Culture");
+          } catch {
+          }
+        mute.roles.remove('732136692085030912').then(member => {
+            message.channel.send(`**${member.user.tag}** a été unmute par **${message.author.tag}**`)
+            
+            client.channels.cache.get('745938396328755220').send(new MessageEmbed()
+              .setColor('#3867d6')
+              .setTitle("Unmute !")
+              .addField('User :', member.user.tag)
+              .addField('Unmuted par :', message.author.tag)
+              .setTimestamp())
+        });
+
+    }
+    
+exports.help = {
+    name: "unmute"
+}
