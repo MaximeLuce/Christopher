3587b2555c3a79f4220dc8a1ab00415d5fb30dd3
Purp1eFox <84629271+Purp1eFox@users.noreply.github.com>
2021-06-05 15:18:24 +0200

    Update send.js
    Changement permissions

--- commands/send.js
+++ commands/send.js
@@ -1,7 +1,7 @@
 const { perms } = require('../config.json')
 
 exports.run = (client, message, args) => {
-  if (!perms.includes(message.author.id)) return message.channel.send("Que voulais-tu faire ? Il n'y a rien à voir ici !")
+  if (!message.member.roles.cache.some(r => r.id == 673268660458094603) && !message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("Que voulais-tu faire ? Il n'y a rien à voir ici !")
 
 
   const arguments = args.splice(1).join(' ')
