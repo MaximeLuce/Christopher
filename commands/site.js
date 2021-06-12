011d899f13bcaa1b4a890b097d39b19835ae7398
Purp1eFox <84629271+Purp1eFox@users.noreply.github.com>
2021-06-12 22:50:25 +0200

    Update site.js

--- commands/site.js
+++ commands/site.js
@@ -4,7 +4,7 @@ exports.run = async (_client, message) => {
   const credits = new MessageEmbed()
     .setTitle(':newspaper: | Site')
     .addField('Lien', 'Pour accéder au menu du Max de Culture, [clique ici !](https://le-max-de-culture.fr/link)')
-    .addField('Crédit', 'Merci beaucoup à toute l\'équipe qui participe Ã  ce projet : \nhttps://le-max-de-culture.fr/fr/team/ \net merci à vous qui prenez part au projet car ce n\'est qu\'avec votre aide que ce dernier pourra prospérer de la meilleure des manières ;)')
+    .addField('Crédit', 'Merci beaucoup à toute l\'équipe qui participe à ce projet : \nhttps://le-max-de-culture.fr/fr/team/ \net merci à vous qui prenez part au projet car ce n\'est qu\'avec votre aide que ce dernier pourra prospérer de la meilleure des manières ;)')
     .setColor('#3867d6')
     .attachFiles(['assets/images/logo.png'])
     .setAuthor('Le Max de Culture', 'attachment://logo.png', 'https://le-max-de-culture.fr/')
